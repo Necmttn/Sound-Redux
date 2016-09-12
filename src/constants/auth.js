c2126469740794eca989b5556894889165725724
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2016-09-12 17:15:54 +0800

    disabled eslint-disable

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -1,2 +1,4 @@
-export const CLIENT_ID = '1fb0d04a94f035059b0424154fd1b18c'
+export const CLIENT_ID = '1fb0d04a94f035059b0424154fd1b18c';
+/*eslint-disable */
 export const REDIRECT_URI = `${window.location.protocol}//${window.location.host}/callback`;
+/*eslint-disable */
